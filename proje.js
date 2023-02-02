1344c18f62b4a3767c10112038e3f450d9a647cd
Swechs <tsadik04@gmail.com>
2023-02-03 00:33:22 +0300

    javascript examples

---
+++ proje.js
@@ -0,0 +1,34 @@
+function hello() {
+    console.log("merhaba")
+    helloword()
+}
+
+function helloword() {
+    console.log("hello world")
+}
+ hello()
+
+ let firstname = 'Sadık'
+
+ function morethings()  {
+    console.log(`merhaba ${firstname}`)
+ } 
+  
+ morethings()
+ 
+ function kamyon(kamyonadı="")  {
+    console.log(`man ${kamyonadı}`)
+ }
+ kamyon("els")
+
+
+
+ function deneme(a,b)   {
+    return a + b ;
+ }
+ alert(deneme(32,878));
+
+ function alerts(alarm) {
+    alert('sadık');
+  }
+  alerts();
